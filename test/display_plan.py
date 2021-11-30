4a2597507388d1986ccc7946a676b42d1a94a450
dxkkxn <ybkstudies@gmail.com>
2021-12-01 00:10:15 +0100

    finish display plan

--- test/display_plan.py
+++ test/display_plan.py
@@ -1,15 +1,31 @@
 import tkinter as tk
 
-def create_rhombus(x:int, y:int):
-    canvas.create_polygon(x,y, x+300, y+100, x, y+200, x-300, y+100,x,y,
-                          outline="red", joinstyle="round")
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
+
+def create_rhombus():
+    # canvas.create_polygon(x,y, x+300, y+100, x, y+200, x-300, y+100,x,y,
+    #                       outline="red", joinstyle="round")
+    global SCREEN_HEIGHT, SCREEN_WIDTH
+    SCREEN_HEIGHT = canvas.winfo_height()
+    SCREEN_WIDTH = canvas.winfo_width()
+    print(SCREEN_HEIGHT, SCREEN_WIDTH)
+    START_Y = SCREEN_HEIGHT/3
+    START_X = SCREEN_WIDTH/2
+    # 10 pixels de marge
+    DIAG_P = SCREEN_HEIGHT-START_Y-10
+    DIAG_G = DIAG_P*2
+    canvas.create_polygon(START_X, START_Y, START_X+DIAG_G/2, START_Y+DIAG_P/2,
+                   START_X, START_Y+DIAG_P, START_X-DIAG_G/2, START_Y+DIAG_P/2)
+    print("created")
 
     return
-def resize(event=None):
+def get_window_size(event=None):
     #print("OOOOOOOOk")
     #print(event)
-    #print(canvas.cget("width")) 
-    #print(canvas.cget("height")) 
+    SCREEN_HEIGTH = root.winfo_height()
+    SCREEN_WIDTH = root.winfo_width()
+#    print(canvas.cget("height"))
     """
     if CANVAS_WIDTH - 2 != event.width :
         CANVAS_WIDTH = event_width-2
@@ -19,29 +35,29 @@ def resize(event=None):
         CANVAS_HEIGHT = event_width-2
         canvas.configure(width=event.width, height=event.height)
     """
+    return
 if __name__ == "__main__" :
     root = tk.Tk()
     root.title("plan")
-    SCREEN_HEIGHT = root.winfo_screenheight()
-    SCREEN_WIDTH = root.winfo_screenwidth()
-    geometry_str = str(SCREEN_WIDTH)+str(SCREEN_HEIGHT)
-    root.geometry(
-    print(root.winfo_screenheight())
-    
-    canvas = tk.Canvas(root, width=SCREEN_WIDTH, height=SCREEN_HEIGHT,
-                       bg="white")
+    top_level = tk.Toplevel()
+    button = tk.Button(top_level, text="display plan", command=create_rhombus, takefocus=True)
+    button.pack(side="top")
+    # SCREEN_HEIGHT = 1080
+    # SCREEN_WIDTH = 1920
+    # geometry_str = str(SCREEN_WIDTH)+str(SCREEN_HEIGHT)
+    # root.geometry()
+    # print(root.winfo_screenheight())
+
+    canvas = tk.Canvas(root, bg="white")
     canvas.pack(side="top", expand=True, fill="both")
-    print(SCREEN_WIDTH, SCREEN_HEIGHT)
-    START_X = SCREEN_WIDTH/2
-    START_Y = SCREEN_HEIGHT/2
-    DIAG_P = START_Y
-    DIAG_G = START_Y*2
-    print(DIAG_P, DIAG_G)
-    canvas.create_polygon(START_X, START_Y, START_X+DIAG_G/2, START_Y+DIAG_P/2,
-                   START_X, START_Y+DIAG_P, START_X-DIAG_G/2, START_Y+DIAG_P/2)
+    # print("-----------")
+    # print(canvas.winfo_height(), canvas.winfo_width())
+    # print(SCREEN_WIDTH, SCREEN_HEIGHT)
     #for i in range(3):
     #    create_rhombus(500-600+i*300, 200+200+i*100)
-    root.bind(sequence="<Configure>", func=resize)
-#    root.attributes('-fullscreen', True)
-    root.resizable(False, False)
+    #root.bind(sequence="<Configure>", func=resize)
+    #root.attributes('-fullscreen', True)
+    root.attributes("-zoomed", True)
+    #print("here->",root.winfo_height())
+    #root.resizable(False, False)
     root.mainloop()
