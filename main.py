159f66a8e1ecd246f040f81f8bbce51b95981417
dxkkxn <ybkstudies@gmail.com>
2021-12-26 19:28:15 +0100

    test in laptop

--- main.py
+++ main.py
@@ -4,28 +4,46 @@ from cube import Cube
 from plan import Plan
 from color import Color
 
+from time import sleep
+
+button_= None
 def display_color_window():
-    color_frame = tk.Frame(root)
+    print(button_)
+    color_frame = tk.Frame(root, bg="red")
     s_height = root.winfo_height()
     x_width = root.winfo_width()
     s_width = root.winfo_width()/3
     id_x = canvas.create_window(x_width, 0, window=color_frame, anchor="ne",
                          height=s_height, width=s_width)
-    canvas.tag_raise("button", id_x)
+    button_.lift(color_frame)
 
+    sleep(10)
+    color_frame.pack_forget()
+
+    print("raised OK")
 def create_plan():
+    global button_
+
     print(scale.get())
+    root.title("xd")
+    root.attributes("-fullscreen", True)
+    print("zoomed")
+    return
     Plan.set_root(root)
     p = Plan(scale.get())
+    initial_frame.place_forget()
     scale.pack_forget()
     button.pack_forget()
-    frame1.pack_forget()
-    frame2.pack_forget()
+
+    return
+
     canvas.pack(side="top", expand=True, fill="both")
     x = root.winfo_width()
+
     button_color = tk.Button(root, bitmap="gray12", command=display_color_window)
-    canvas.create_window(x, 0, window=button_color, anchor="ne", tags="button")
-    button_color.bind()
+    button_ = canvas.create_window(x, 0, window=button_color, anchor="ne", tags="button")
+    button_ = button_color
+    print(button_)
     return
 
 def create_cube(event=None):
@@ -46,23 +64,46 @@ def create_cube(event=None):
 
     cube = Cube(relative_pos, color, "xd")
 
+
 if __name__ == "__main__" :
     root = tk.Tk()
     root.title("plan")
-    canvas = tk.Canvas(root, bg="white")
-    button = tk.Button(root, text="display plan", 
+
+    main_frame = tk.Frame(root, bg="yellow", width=960, height=540)
+    initial_frame = tk.Frame(main_frame, bg="green", width=1920/2, height=1080/2)
+
+    canvas = tk.Canvas(main_frame, bg="white")
+    button = tk.Button(initial_frame, text="display plan",
                        command= create_plan, takefocus=True)
-    scale = tk.Scale(root, from_=1, to=30, repeatinterval=1, 
-                     orient="horizontal", label="Taille", tickinterval=1)
+    scale = tk.Scale(initial_frame, from_=1, to=50, repeatinterval=5,
+                     orient="horizontal", label="Taille", tickinterval=2,
+                     length=800)
+
+
     Rhombus.set_canvas(canvas)
     Cube.set_canvas(canvas)
 
-    frame1 = tk.Frame(root)
-    frame2 = tk.Frame(root)
+    # root.geometry("960x540")
+    # root.attributes("-zoom", True)
+
     canvas.bind(sequence="<ButtonPress-1>", func= create_cube)
-    frame1.pack(fill="y", expand=True)
-    scale.pack(side="top", anchor="center", fill="both")
-    button.pack(anchor="center")
-    frame2.pack(fill="y", expand=True)
-    root.attributes("-zoomed", True)
+
+    main_frame.pack(fill="both", expand=True)
+    initial_frame.place(in_=main_frame, anchor="center", relx=0.5, rely=.5)
+    scale.pack(side="top", fill="both", expand=True )
+    button.pack(side="right")
+
+    # s_height = root.winfo_height()
+    # x_width = root.winfo_width()
+    # print(s_height, x_width)
+    #
+    # f1 = tk.Frame(width=200, height=200, background="red")
+    # f2 = tk.Frame(width=100, height=100, background="blue")
+
+    # f1.pack(fill="both", expand=True, padx=20, pady=20)
+    # f2.place(in_=f1, anchor="c", relx=.5, rely=.5)
+
     root.mainloop()
+    # scale.grid(row = 0)
+    # button.grid(row=1 )
+
