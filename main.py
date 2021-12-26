41110d9c52d38de055f166e1334f6a96aa68baab
dxkkxn <ybkstudies@gmail.com>
2021-12-27 00:35:08 +0100

    test in laptop

--- main.py
+++ main.py
@@ -5,44 +5,83 @@ from plan import Plan
 from color import Color
 
 from time import sleep
+from PIL import Image, ImageTk
+
+def change_col(event, frame):
+    frame.configure(bg="cyan")
 
 button_= None
 def display_color_window():
     print(button_)
-    color_frame = tk.Frame(root, bg="red")
     s_height = root.winfo_height()
     x_width = root.winfo_width()
     s_width = root.winfo_width()/3
-    id_x = canvas.create_window(x_width, 0, window=color_frame, anchor="ne",
-                         height=s_height, width=s_width)
-    button_.lift(color_frame)
+    color_frame = tk.Frame(root, bg="red",height=s_height, width=s_width)
 
-    sleep(10)
-    color_frame.pack_forget()
+    color_frame.place(in_=main_frame, x=1920, y=0, anchor="ne")
 
-    print("raised OK")
+    button_frame =  tk.Frame(color_frame)
+    tk.Button(button_frame, bitmap="error" ).pack(side="right")
+    button_frame.pack(side="top")
+    image_x = Image.open("./img/blue.jpg")
+
+
+    img = ImageTk.PhotoImage(image_x)
+
+    tk.Button(color_frame, image=img,  width=160, height=160, highlightcolor="cyan", highlightthickness=30,
+               relief="raised", overrelief="raised", padx=70, pady=70).pack(side="top")
+
+    colors = ["yellow", "red", "pink", "black", "white", "blue", "brown",
+              "cyan", "DarkBlue", "green", "orange"]
+    btn = []
+
+    rw = 0
+    col = 0
+    # for color in colors:
+    #     if rw%3 == 0:
+    #         container_frame = tk.Frame(color_frame)
+    #         container_frame.pack(side="top", fill="x")
+
+    #     curr_frame = tk.Frame(container_frame, relief="raised", highlightcolor="cyan", bd=4,
+    #              padx=50, pady=50, bg=color, width=s_width/4,
+    #              height=s_width/4)
+    #     curr_frame.pack(side="left")
+    #     curr_frame.bind(sequence="<ButtonPress-1>", func= lambda x : change_col(x, curr_frame))
+
+    #     rw+=1
+
+
+    # btn[0].grid(row=1, column=0)
+    # btn[1].grid(row=1, column=1)
+    # btn[2].grid(row=1, column=2)
+    # btn[3].grid(row=2, column=0)
+
+
+
+
+    # id_x = canvas.create_window(x_width, 0, window=color_frame, anchor="ne",
+    #                      height=s_height, width=s_width)
+    # button_.lift(color_frame)
+    color_frame.lift(canvas)
+
+
 def create_plan():
     global button_
 
     print(scale.get())
-    root.title("xd")
-    root.attributes("-fullscreen", True)
-    print("zoomed")
-    return
+    root.attributes("-zoom", True)
     Plan.set_root(root)
     p = Plan(scale.get())
     initial_frame.place_forget()
     scale.pack_forget()
     button.pack_forget()
-
-    return
-
     canvas.pack(side="top", expand=True, fill="both")
-    x = root.winfo_width()
+    x_x = root.winfo_width()
 
-    button_color = tk.Button(root, bitmap="gray12", command=display_color_window)
-    button_ = canvas.create_window(x, 0, window=button_color, anchor="ne", tags="button")
-    button_ = button_color
+    button_color = tk.Button(main_frame, bitmap="gray12", command=display_color_window)
+    button_color.place(in_=main_frame, x=x_x, y=0, anchor="ne")
+    # button_ = canvas.create_window(x, 0, window=button_color, anchor="ne", tags="button")
+    # button_ = button_color
     print(button_)
     return
 
@@ -70,7 +109,7 @@ if __name__ == "__main__" :
     root.title("plan")
 
     main_frame = tk.Frame(root, bg="yellow", width=960, height=540)
-    initial_frame = tk.Frame(main_frame, bg="green", width=1920/2, height=1080/2)
+    initial_frame = tk.Frame(main_frame, bg="green")
 
     canvas = tk.Canvas(main_frame, bg="white")
     button = tk.Button(initial_frame, text="display plan",
@@ -84,7 +123,8 @@ if __name__ == "__main__" :
     Cube.set_canvas(canvas)
 
     # root.geometry("960x540")
-    # root.attributes("-zoom", True)
+    root.attributes("-zoom", True)
+    root.resizable(False, False)
 
     canvas.bind(sequence="<ButtonPress-1>", func= create_cube)
 
